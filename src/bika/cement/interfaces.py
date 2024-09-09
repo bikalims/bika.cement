9118967f63dfee5ce8c49f50943db3642a4fa1ae
Luthando Ndlela <luthando.ndlela1@gmail.com>
2024-09-09 14:10:46 +0200

    Add Material Types to lims setup

--- src/bika/cement/interfaces.py
+++ src/bika/cement/interfaces.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
 """Module where all interfaces, events and exceptions live."""
 
+
+from zope.interface import Interface
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 
 
 class IBikaCementLayer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
+
+
+class IMaterialTypeFolder(Interface):
+    """Marker interface for material type setup folder
+    """
--- src/bika/cement/setuphandlers.py
+++ src/bika/cement/setuphandlers.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from Products.CMFPlone.interfaces import INonInstallable
 from zope.interface import implementer
+from bika.cement.config import PROFILE_ID
+from bika.cement.config import logger
+from bika.lims import api
+from senaite.core.setuphandlers import add_dexterity_items
 
 
 @implementer(INonInstallable)
@@ -16,8 +20,29 @@ class HiddenProfiles(object):
 def post_install(context):
     """Post install script"""
     # Do something at the end of the installation of this package.
+    logger.info("BIKA.CEMENT post install handler [BEGIN]")
+    profile_id = PROFILE_ID
+    context = context._getImportContext(profile_id)
+    portal = context.getSite()
+    add_dexterity_setup_items(portal)
 
 
 def uninstall(context):
     """Uninstall script"""
     # Do something at the end of the uninstallation of this package.
+
+
+def add_dexterity_setup_items(portal):
+    """Adds the Dexterity Container in the Setup Folder
+
+    N.B.: We do this in code, because adding this as Generic Setup Profile in
+          `profiles/default/structure` flushes the contents on every import.
+    """
+    # Tuples of ID, Title, FTI
+    items = [
+        ("materialtype_folder",
+         "Material Types",
+         "MaterialTypeFolder"),
+    ]
+    setup = api.get_setup()
+    add_dexterity_items(setup, items)
