3b5fc5ccd9bec060c482541bd2fc03f282097c2f
DanE417 <daanyaaleffendi1997@gmail.com>
2024-09-09 14:36:02 +0200

    Added Material Class to lims setup

--- src/bika/cement/interfaces.py
+++ src/bika/cement/interfaces.py
@@ -2,7 +2,16 @@
 """Module where all interfaces, events and exceptions live."""
 
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
-
+from zope.interface import Interface
 
 class IBikaCementLayer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
+
+class IMaterialClass(Interface):
+    """Marker interface for material class
+    """
+
+
+class IMaterialClassFolder(Interface):
+    """Marker interface for material class setup folder
+    """
--- src/bika/cement/setuphandlers.py
+++ src/bika/cement/setuphandlers.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
+from bika.cement.config import _
+from bika.cement.config import PROFILE_ID
+from bika.cement.config import logger
+from bika.lims import api
+from senaite.core.setuphandlers import add_dexterity_items
 from Products.CMFPlone.interfaces import INonInstallable
 from zope.interface import implementer
 
@@ -16,8 +21,26 @@ class HiddenProfiles(object):
 def post_install(context):
     """Post install script"""
     # Do something at the end of the installation of this package.
+    logger.info("BIKA.AQUACULTURE post install handler [BEGIN]")
+    profile_id = PROFILE_ID
+    context = context._getImportContext(profile_id)
+    portal = context.getSite()
+    add_dexterity_setup_items(portal)
 
 
 def uninstall(context):
     """Uninstall script"""
     # Do something at the end of the uninstallation of this package.
+
+def add_dexterity_setup_items(portal):
+    """Adds the Dexterity Container in the Setup Folder
+
+    N.B.: We do this in code, because adding this as Generic Setup Profile in
+          `profiles/default/structure` flushes the contents on every import.
+    """
+    # Tuples of ID, Title, FTI
+    items = [
+        ("material_class_folder", "Material Class", "MaterialClassFolder"),
+    ]
+    setup = api.get_setup()
+    add_dexterity_items(setup, items)
