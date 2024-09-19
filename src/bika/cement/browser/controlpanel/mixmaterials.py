3e2c1c9ae17fb583ed66865b8e14620a04557bc4
DanE417 <daanyaaleffendi1997@gmail.com>
2024-09-20 01:45:24 +0200

    Rename mix material and material class folders

--- src/bika/cement/browser/controlpanel/mixmaterials.py
+++ src/bika/cement/browser/controlpanel/mixmaterials.py
@@ -22,48 +22,60 @@ import collections
 
 from bika.cement.config import _
 from bika.lims import api
-from bika.lims.utils import get_link_for
+from bika.lims.utils import get_link, get_link_for
 from senaite.app.listing import ListingView
 from senaite.core.catalog import SETUP_CATALOG
 
 
-class MaterialClassFolderView(ListingView):
+class MixMaterialsView(ListingView):
     """Displays all available sample containers in a table
     """
 
     def __init__(self, context, request):
-        super(MaterialClassFolderView, self).__init__(context, request)
+        super(MixMaterialsView, self).__init__(context, request)
+
         self.catalog = SETUP_CATALOG
 
         self.contentFilter = {
-            "portal_type": "MaterialClass",
-            "sort_on": "sort_key",
-            "sort_order": "ascending",
+            "portal_type": "MixMaterial",
+            "sort_on": "sortable_title",
         }
 
         self.context_actions = {
             _("Add"): {
-                "url": "++add++MaterialClass",
+                "url": "++add++MixMaterial",
                 "icon": "++resource++bika.lims.images/add.png",
             }}
 
         t = self.context.translate
-        self.title = t(_("Material Classes"))
+        self.title = t(_("Mix Materials"))
         self.description = t(_(""))
 
         self.show_select_column = True
         self.pagesize = 25
 
         self.columns = collections.OrderedDict((
-            ("sort_key", {
-                "title": _("Sort Key"),
-                "index": "sort_key"}),
             ("title", {
                 "title": _("Title"),
+                "index": "sortable_title"}),
+            ("material_type", {
+                "title": _("Material Type"),
+                "index": "material_type"}),
+            ("manufacturer", {
+                "title": _("Manufacturer"),
+                "index": "sortable_title"}),
+            ("supplier", {
+                "title": _("Supplier"),
                 "index": "sortable_title"}),
             ("description", {
                 "title": _("Description"),
                 "index": "description"}),
+            ("specific_gravity", {
+                "title": _("Specific Gravity"),
+                "index": "specific_gravity"}),
+            ("absorption_rate", {
+                "title": _("Absorption Rate"),
+                "index": "absorption_rate"}),
         ))
 
         self.review_states = [
@@ -98,6 +110,43 @@ class MaterialClassFolderView(ListingView):
 
         item["replace"]["title"] = get_link_for(obj)
         item["description"] = api.get_description(obj)
-        item["sort_key"] = obj.sort_key
+        item["specific_gravity"] = obj.specific_gravity
+        item["absorption_rate"] = obj.absorption_rate
+
+        # Manufacturer
+        manufacturer_list = obj.manufacturer
+        if manufacturer_list:
+            manufacturer_obj = api.get_object_by_uid(manufacturer_list[0])
+            manufacturer_title = manufacturer_obj.title
+            manufacturer_url = manufacturer_obj.absolute_url()
+            manufacturer_link = get_link(
+                manufacturer_url, manufacturer_title
+            )
+            item["manufacturer"] = manufacturer_title
+            item["replace"]["manufacturer"] = manufacturer_link
+
+        # Supplier
+        supplier_list = obj.supplier
+        if supplier_list:
+            supplier_obj = api.get_object_by_uid(supplier_list[0])
+            supplier_title = supplier_obj.title
+            supplier_url = supplier_obj.absolute_url()
+            supplier_link = get_link(
+                supplier_url, supplier_title
+            )
+            item["supplier"] = supplier_title
+            item["replace"]["supplier"] = supplier_link
+
+        # Material Type
+        material_type_list = obj.material_type
+        if material_type_list:
+            material_type_obj = api.get_object_by_uid(material_type_list[0])
+            material_type_title = material_type_obj.title
+            material_type_url = material_type_obj.absolute_url()
+            material_type_link = get_link(
+                material_type_url, material_type_title
+            )
+            item["material_type"] = material_type_title
+            item["replace"]["material_type"] = material_type_link
 
         return item
