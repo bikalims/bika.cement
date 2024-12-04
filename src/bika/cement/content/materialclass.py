abca1ff02d0c5e807d30f9f10db6c2d0b21d2283
Luthando Ndlela <104898641+luthando5@users.noreply.github.com>
2024-12-04 10:36:31 +0200

    Mix materials content types (#31)
    * Add Material Types to lims setup
    
    * add description
    
    * Added Material Class to LIMS setup and added view page
    
    * PEP8 styling
    
    * Change to Material Classes
    
    * Resolve conflicts on mix materials branch merge
    
    * Add curing methods to LIMS Setup
    
    * Add mix type content type
    
    * Add Sort order to material classes
    
    * make sort key default sort
    
    * PEP 8
    
    * change sort order to ascending
    
    * add material class drop down to material types
    
    * Add mix material content type
    
    * Made requested changes
    
    * -Rename Curing Method Folder
    -Add curing methods to senaite setup instead of bika setup
    
    * fix typo
    
    * Add curing method content type test
    
    * Rename mix material and material class folders
    
    * fixed an error
    
    * * Rename Material Types and Mix Types content types
    
    * Add Schema string to xml files
    
    * Point directly to interfaces in the controlpanel config
    
    * Title field required
    
    * Import api from bika.lims instead of senaite
    
    * Add tests for material types and mix types
    
    * Add tests for material classes and mix materials
    
    * Change mix type sort order to ascending
    
    * Fix sort order on material types listing
    Fix sort order on material types lookup on material class creation
    Fix sort order on supplier lookup on mix material creation
    
    * Add deactivate workflow for all content types
    
    ---------
    
    Co-authored-by: Daanyaal Effendi <70233449+DanE417@users.noreply.github.com>
    Co-authored-by: DanE417 <daanyaaleffendi1997@gmail.com>

---
+++ src/bika/cement/content/materialclass.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+from AccessControl import ClassSecurityInfo
+from bika.cement.interfaces import IMaterialClass
+from bika.lims.interfaces import IDeactivable
+from plone.dexterity.content import Container
+from plone.supermodel import model
+from senaite.core.catalog import SETUP_CATALOG
+from bika.lims import api
+from zope.interface import implementer
+from zope import schema
+
+
+class IMaterialClassSchema(model.Schema):
+    """Marker interface and Dexterity Python Schema for MaterialClass"""
+
+    title = schema.TextLine(
+        title=u"Title",
+        required=True,
+    )
+
+    description = schema.Text(
+        title=u"Description",
+        required=False,
+    )
+
+    sort_key = schema.Int(
+        title=u"Sort Key",
+        required=True,
+    )
+
+
+@implementer(IMaterialClass, IMaterialClassSchema, IDeactivable)
+class MaterialClass(Container):
+    """Content-type class for IMaterialClass"""
+
+    _catalogs = [SETUP_CATALOG]
+
+    security = ClassSecurityInfo()
+
+    @security.private
+    def accessor(self, fieldname):
+        """Return the field accessor for the fieldname"""
+        schema = api.get_schema(self)
+        if fieldname not in schema:
+            return None
+        return schema[fieldname].get
+
+    @security.private
+    def mutator(self, fieldname):
+        """Return the field mutator for the fieldname"""
+        schema = api.get_schema(self)
+        if fieldname not in schema:
+            return None
+        result = schema[fieldname].set
+        self.reindexObject()
+        return result
--- src/bika/cement/setuphandlers.py
+++ src/bika/cement/setuphandlers.py
@@ -1,23 +1,68 @@
 # -*- coding: utf-8 -*-
 from Products.CMFPlone.interfaces import INonInstallable
 from zope.interface import implementer
+from bika.cement.config import PROFILE_ID
+from bika.cement.config import logger
+from bika.lims import api
+from senaite.core.catalog import SETUP_CATALOG
+from senaite.core.setuphandlers import (
+    add_dexterity_items,
+    setup_other_catalogs,
+)
+
+
+INDEXES = [
+    (SETUP_CATALOG, "sort_key", "", "FieldIndex"),
+]
+
+COLUMNS = [
+    (SETUP_CATALOG, "sort_key"),
+]
 
 
 @implementer(INonInstallable)
 class HiddenProfiles(object):
-
     def getNonInstallableProfiles(self):
         """Hide uninstall profile from site-creation and quickinstaller."""
         return [
-            'bika.cement:uninstall',
+            "bika.cement:uninstall",
         ]
 
 
 def post_install(context):
     """Post install script"""
     # Do something at the end of the installation of this package.
+    logger.info("BIKA.CEMENT post install handler [BEGIN]")
+    profile_id = PROFILE_ID
+    context = context._getImportContext(profile_id)
+    portal = context.getSite()
+    add_dexterity_setup_items(portal)
+    setup_catalogs(portal)
 
 
 def uninstall(context):
     """Uninstall script"""
     # Do something at the end of the uninstallation of this package.
+
+
+def add_dexterity_setup_items(portal):
+    """Adds the Dexterity Container in the Setup Folder
+
+    N.B.: We do this in code, because adding this as Generic Setup Profile in
+          `profiles/default/structure` flushes the contents on every import.
+    """
+    # Tuples of ID, Title, FTI
+    items = [
+        ("materialtypes", "Material Types", "MaterialTypes"),
+        ("materialclasses", "Material Classes", "MaterialClasses"),
+        ("curingmethods", "Curing Methods", "CuringMethods"),
+        ("mixtypes", "Mix Types", "MixTypes"),
+        ("mixmaterials", "Mix Materials", "MixMaterials"),
+    ]
+    setup = api.get_senaite_setup()
+    add_dexterity_items(setup, items)
+
+
+def setup_catalogs(portal):
+    """Setup catalogs"""
+    setup_other_catalogs(portal, indexes=INDEXES, columns=COLUMNS)
