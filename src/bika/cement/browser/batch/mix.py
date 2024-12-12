1936343ad0f93e7eab0be64738784bc3436a8f7b
Lunga Baliwe <lunga001@gmail.com>
2024-12-12 22:46:52 +0200

    Started with Material Listing on the Mix View

--- src/bika/cement/browser/batch/mix.py
+++ src/bika/cement/browser/batch/mix.py
@@ -5,10 +5,14 @@ from plone.app.contentlisting.interfaces import IContentListing
 from plone.app.layout.globals.interfaces import IViewView
 from zope.interface import alsoProvides
 from zope.interface import implements
+from plone.app.layout.viewlets import ViewletBase
 
 from bika.lims import api
 from bika.lims.browser import BrowserView
 from senaite.core.catalog import SETUP_CATALOG
+
+from bika.cement.config import _
+from bika.cement.browser.controlpanel.mixmaterials import MixMaterialsView as MMV
 
 
 class BatchMixView(BrowserView):
@@ -45,3 +49,48 @@ class BatchMixView(BrowserView):
         brains = api.search(query, SETUP_CATALOG)
         if len(brains) == 1:
             return api.get_object(brains[0])
+
+
+class MixMaterialViewlet(ViewletBase):
+    """Laboratory analyses section viewlet for Sample view
+    """
+    index = ViewPageTemplateFile("templates/mixmaterials.pt")
+
+    title = _("Materials")
+    icon_name = "analysisservice"
+    capture = "lab"
+
+    @property
+    def batch(self):
+        return self.context
+
+    def is_collapsed(self):
+        return False
+
+    def available(self):
+        """Returns true if this sample contains at least one analysis for the
+        point of capture (capture)
+        """
+        return True
+
+    def get_listing_view(self):
+        request = api.get_request()
+        view_name = "table_mix_material"
+        view = api.get_view(view_name, context=self.batch, request=request)
+        return view
+
+    def contents_table(self):
+        view = self.get_listing_view()
+        view.update()
+        view.before_render()
+        return view.ajax_contents_table()
+
+
+class MixMaterialTable(MMV):
+    """Displays all available sample containers in a table
+    """
+
+    def __init__(self, context, request):
+        super(MixMaterialTable, self).__init__(context, request)
+
+        self.show_search = False
