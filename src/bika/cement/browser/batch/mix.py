d502fbe6248295555fc5afd84d47296b0ada3b2c
Luthando Ndlela <luthando.ndlela1@gmail.com>
2024-12-17 02:22:57 +0200

    Create concrete mix design parser

--- src/bika/cement/browser/batch/mix.py
+++ src/bika/cement/browser/batch/mix.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import openpyxl
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.contentlisting.interfaces import IContentListing
 from plone.app.layout.globals.interfaces import IViewView
@@ -27,6 +28,19 @@ class BatchMixView(BrowserView):
         alsoProvides(request, IContentListing)
 
     def __call__(self):
+        batch = self.context
+        mix_template_file = batch.MixTemplateFile
+        if not mix_template_file:
+            return self.template()
+        blob_file = mix_template_file.blob
+        sheet_name = "SSD & Batch values"
+        data = self.get_data_from_blob_file(blob_file, sheet_name)
+        mix_design_data = self.parse_mix_design_data(data)
+        concrete_data = self.parse_mix_design_concrete_data(data)
+        if self.get_mix_design():
+            return self.template()
+        mix_design = self.create_mix_design(mix_design_data)
+        self.create_concrete_mix_design(mix_design, concrete_data)
         return self.template()
 
     def get_mix_design(self):
@@ -52,6 +66,86 @@ class BatchMixView(BrowserView):
         brains = api.search(query, SETUP_CATALOG)
         if len(brains) == 1:
             return api.get_object(brains[0])
+
+    def get_data_from_blob_file(self, blob, sheet_name=None):
+        # Step 1: Open the blob
+        with blob.open('r') as blob_file:
+            workbook = openpyxl.load_workbook(blob_file, data_only=True, read_only=True)
+
+            # Step 2: Get the specific sheet
+            if sheet_name:
+                sheet = workbook[sheet_name]  # Access by name
+            else:
+                sheet = workbook.active  # Default to the first sheet
+
+            # Step 3: Extract data
+            data = []
+            coa = False
+            for row in sheet.iter_rows(values_only=True):
+                if coa and any(row):
+                    data.append(row)
+                if row:
+                    if row[0] == "COA":
+                        coa = True
+            return data
+
+    def parse_mix_design_data(self, data):
+        mix_design_data = {}
+        mix_design_data["title"] = data[0][2]
+        mix_design_data["project"] = data[0][2]
+        mix_design_data["mix_design_type"] = data[0][4]
+        # to-do: (mix_materials)
+        return mix_design_data
+
+    def parse_mix_design_concrete_data(self, data):
+        concrete_data = {}
+        concrete_data["title"] = data[0][2]
+        concrete_data["batch_volume"] = data[0][6]
+        concrete_data["design"] = data[1][2]  # design number
+        concrete_data["replacement"] = data[1][4]
+        concrete_data["paste_content"] = data[1][6]
+        # missing design w/cm
+        concrete_data["total_cm"] = data[2][4]
+        # missing theoretical volume
+        concrete_data["super_air_meter"] = data[2][8]
+        concrete_data["design_air"] = data[3][2]
+        concrete_data["design_slump"] = data[3][4]
+        concrete_data["theoretical_unit_weight"] = data[3][6]
+        concrete_data["measured_air"] = data[4][2]
+        concrete_data["measured_slump"] = data[4][4]
+        # date missing
+        concrete_data["lab_temperature"] = data[5][4]
+        # missing time
+        concrete_data["concrete_temp"] = data[6][4]
+        return concrete_data
+
+    def create_mix_design(self, data):
+        batch = self.context
+        mix_design = api.create(batch, "MixDesign")
+        mix_design.title = data.get("title")
+        mix_design.project = data.get("project")
+        mix_design.mix_design_type = data.get("mix_design_type")
+        # mix_design.edit(**data)
+        return mix_design
+
+    def create_concrete_mix_design(self, mix_design, data):
+        concrete_mix_design = api.create(mix_design, "MixDesignConcrete")
+        concrete_mix_design.title = data.get("title")
+        concrete_mix_design.batch_volume = data.get("batch_volume")
+        concrete_mix_design.design = data.get("design")
+        concrete_mix_design.replacement = data.get("replacement")
+        concrete_mix_design.paste_content = data.get("paste_content")
+        concrete_mix_design.total_cm = data.get("total_cm")
+        concrete_mix_design.super_air_meter = data.get("super_air_meter")
+        concrete_mix_design.design_air = data.get("design_air")
+        concrete_mix_design.design_slump = data.get("design_slump")
+        concrete_mix_design.theoretical_unit_weight = data.get("theoretical_unit_weight")
+        concrete_mix_design.measured_air = data.get("measured_air")
+        concrete_mix_design.measured_slump = data.get("measured_slump")
+        concrete_mix_design.lab_temperature = data.get("lab_temperature")
+        concrete_mix_design.concrete_temp = data.get("concrete_temp")
+        # concrete_mix_design.edit(**data)
+        return concrete_mix_design
 
 
 class MixMaterialViewlet(ViewletBase):
